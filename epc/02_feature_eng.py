cb1e385e0d93ffc774e70150be8effc9bf7a01b0
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-05-23 14:25:22 +0100

    move epc to its own folder

---
+++ epc/02_feature_eng.py
@@ -0,0 +1,317 @@
+# Feature engineering the the downloaded data
+
+# This approach is informed by the discussion here
+# https://github.com/moj-analytical-services/splink/discussions/2022
+
+# We will separately parse out 'house number/name' tokens so that we can build
+# more sophisticated logic for those fields
+
+
+# NOTE:  To understand each step of the transform, you can call .df() on the result
+# of duckdb.sql(sql) e.g.
+# df_pp_fields_concat = duckdb.sql(sql)
+# then add
+# display(df_pp_fields_concat.df()) to see the intermediate result
+
+# See https://github.com/ONSdigital/address-index-data/blob/fbd434bd3c3f3b08881d2ef9e2797a62ee5eae84/DataScience/ProbabilisticParser/common/tokens.py#L1
+# for some additional ideas (not implemented here) about how to tokenise
+import os
+
+import duckdb
+import pandas as pd
+
+pd.options.display.max_columns = None
+pd.options.display.max_colwidth = None
+
+
+df_pp = pd.read_parquet("./raw_address_data/price_paid_addresses.parquet")
+print(f"Number of records in price paid data: {len(df_pp):,.0f}")
+df_epc = pd.read_parquet("./raw_address_data/adur_epc.parquet")
+print(f"Number of records in epc data: {len(df_epc):,.0f}")
+
+
+# district, county fields exist but probably not useful
+sql = """
+select transaction_unique_identifier as unique_id,
+    'price_paid' as source_dataset,
+    paon, saon, street, locality, town_city,  postcode
+from df_pp
+"""
+
+
+df_pp_address_fields = duckdb.sql(sql)
+
+sql = """
+select
+    unique_id,
+    source_dataset,
+    upper(concat(saon, ' ', paon, ' ', street, ' ', locality, ' ', town_city)) as address_concat,
+    postcode
+from df_pp_address_fields
+"""
+df_pp_fields_concat = duckdb.sql(sql)
+
+# LOCAL_AUTHORITY_LABEL exists, but isn't really useful
+sql = """
+select
+LMK_KEY as unique_id,
+'epc' as source_dataset,
+address1, address2, address3, POSTTOWN,  postcode
+from df_epc
+
+"""
+df_epc_address_fields = duckdb.sql(sql)
+
+sql = """
+select
+    unique_id,
+    source_dataset,
+    upper(concat(address1, ' ', address2, ' ', address3, ' ', POSTTOWN)) as address_concat,
+    postcode
+from df_epc_address_fields
+"""
+
+df_epc_fields_concat = duckdb.sql(sql)
+df_epc_fields_concat.df()
+
+sql = """
+select
+    unique_id,
+    source_dataset,
+    address_concat,
+    postcode
+from df_epc_fields_concat
+union all
+select
+
+    unique_id,
+    source_dataset,
+    address_concat,
+    postcode
+from df_pp_fields_concat
+
+"""
+df_vertical_concat = duckdb.sql(sql)
+
+# TODO: Could use more advance regex here to e.g. parse out numbers better
+# Examples that we could better deal with:
+# 'Flat 1 A' - detect that the 'number' is 1A, and this isn't two separate tokens
+# '10-20 bridge street' or '10 - 20 bridge street'  - extract the number as '10-20'
+
+
+sql = """
+SELECT
+    unique_id,
+    source_dataset,
+    trim(regexp_replace(
+            regexp_replace(
+                regexp_replace(address_concat,',', ' ', 'g'),
+                '\\s*\\-\\s*', '-', 'g'), -- useful for cases like '10 - 20 bridge st'
+            '[^a-zA-Z0-9 -]', '', 'g'  -- Remove anything that isn't a-zA-Z0-9 or space
+        )
+    ) AS address_concat_cleaned,
+    address_concat,
+    postcode
+FROM df_vertical_concat
+"""
+
+
+df_all_concat_punc = duckdb.query(sql)
+
+# Pattern to detect tokens with numbers in them inc e.g. 10-20
+
+regex_pattern = r"\b\d*[\w\-]*\d+[\w\-]*\d*\b"
+
+sql = f"""
+SELECT
+    unique_id,
+    source_dataset,
+    address_concat,
+    regexp_replace(address_concat_cleaned, '{regex_pattern}', '', 'g') AS address_without_numbers,
+    postcode,
+    regexp_extract_all(address_concat_cleaned, '{regex_pattern}') AS numeric_tokens
+FROM df_all_concat_punc
+"""
+
+
+df_all_numbers_in_array = duckdb.query(sql)
+
+
+sql = """
+select
+    unique_id,
+    source_dataset,
+    address_concat,
+    numeric_tokens[1] as numeric_token_1,
+    numeric_tokens[2] as numeric_token_2,
+    numeric_tokens[3] as numeric_token_3,
+    trim(regexp_replace(address_without_numbers, '\\s+', ' ', 'g'))
+        as address_without_numbers,
+postcode
+from df_all_numbers_in_array
+"""
+
+df_all_numbers_as_cols = duckdb.sql(sql)
+
+
+sql = """
+select
+    unique_id,
+    source_dataset,
+    address_concat,
+numeric_token_1,
+numeric_token_2,
+numeric_token_3,
+regexp_split_to_array(trim(address_without_numbers), '\\s') as address_without_numbers_tokenised,
+postcode
+from df_all_numbers_as_cols
+"""
+df_all_numbers_as_cols_others_tokenised = duckdb.sql(sql)
+
+# If no numeric tokens, make the first token a 'number' because it's likely to be a house name
+sql = """
+select
+    unique_id,
+    source_dataset,
+    address_concat,
+    case
+        when numeric_token_1 is null then address_without_numbers_tokenised[1]
+        else numeric_token_1
+        end as numeric_token_1,
+    numeric_token_2,
+    numeric_token_3,
+    case
+        when numeric_token_1 is null then address_without_numbers_tokenised[2:]
+        else address_without_numbers_tokenised
+    end
+    as address_without_numbers_tokenised,
+    postcode
+from df_all_numbers_as_cols_others_tokenised
+"""
+
+df_all_numbers_as_cols_others_tokenised_2 = duckdb.sql(sql)
+
+# Make tokens distinct
+sql = """
+select
+    unique_id,
+    source_dataset,
+    address_concat,
+numeric_token_1,
+numeric_token_2,
+numeric_token_3,
+list_distinct(address_without_numbers_tokenised) as address_without_numbers_tokenised,
+postcode
+from df_all_numbers_as_cols_others_tokenised_2
+"""
+df_all_numbers_as_cols_others_tokenised_distinct = duckdb.sql(sql)
+
+# Compute relative term frequencies amongst the tokens
+sql = """
+select
+    token,
+    count(*)  / sum(count(*)) over() as relative_frequency
+from (
+    select
+        unnest(address_without_numbers_tokenised) as token
+    from df_all_numbers_as_cols_others_tokenised_distinct
+)
+group by token
+order by relative_frequency desc
+"""
+token_counts = duckdb.sql(sql)
+
+# Add relative term frequcnies to token array
+sql = """
+with
+addresses_exploded as (
+select
+    unique_id, unnest(address_without_numbers_tokenised) as token
+from df_all_numbers_as_cols_others_tokenised_distinct),
+address_groups as (
+select addresses_exploded.*, token_counts.relative_frequency
+from addresses_exploded
+left join token_counts
+on addresses_exploded.token = token_counts.token
+)
+select
+    unique_id,
+    list(struct_pack(token := token, relative_frequency := relative_frequency)) as token_relative_frequency_arr
+from address_groups
+group by unique_id
+
+"""
+adresses_with_token_relative_frequency = duckdb.sql(sql)
+
+
+sql = """
+select
+    d.unique_id,
+    d.source_dataset,
+    d.address_concat,
+    d.numeric_token_1,
+    d.numeric_token_2,
+    d.numeric_token_3,
+    r.token_relative_frequency_arr,
+    d.postcode
+from
+df_all_numbers_as_cols_others_tokenised_distinct as d
+inner join adresses_with_token_relative_frequency as r
+on d.unique_id = r.unique_id
+
+"""
+
+address_with_token_relative_frequency_arr = duckdb.sql(sql)
+
+# Split into common and common tokens, retain token frequency only for uncommon
+sql = """
+select
+    unique_id,
+    source_dataset,
+    address_concat,
+    numeric_token_1,
+    numeric_token_2,
+    numeric_token_3,
+    array_filter(token_relative_frequency_arr, x -> x.relative_frequency < 0.01) as token_relative_frequency_arr,
+    array_transform(
+        array_filter(token_relative_frequency_arr, x -> x.relative_frequency >= 0.01),
+        x -> x.token
+        ) as common_tokens,
+    postcode
+from address_with_token_relative_frequency_arr
+"""
+
+final = duckdb.sql(sql)
+
+
+if not os.path.exists("splink_in"):
+    os.makedirs("splink_in")
+
+
+sql = """
+COPY (
+select * from final
+where source_dataset = 'price_paid'
+) TO 'splink_in/price_paid.parquet' (FORMAT PARQUET);
+"""
+duckdb.sql(sql)
+
+sql = """
+COPY (
+select * from final
+where source_dataset = 'epc'
+) TO 'splink_in/epc.parquet' (FORMAT PARQUET);
+"""
+duckdb.sql(sql)
+
+# Inspect results
+# Find a record where numeric_token_1 does not contain any 0-9 using regex in duckdb
+# sql = """
+# select * from
+# read_parquet('splink_in/price_paid.parquet')
+# where
+# not regexp_matches(numeric_token_1, '[0-9]')
+# order by random()
+# limit 10
+# """
+# duckdb.sql(sql).df()
---
+++ epc/03_splink_model.py
@@ -0,0 +1,305 @@
+# Going to largely follow
+# https://github.com/moj-analytical-services/splink/discussions/2022
+import random
+
+import duckdb
+import pandas as pd
+import splink.duckdb.comparison_level_library as cll
+import splink.duckdb.comparison_library as cl
+from IPython.display import display
+from splink.duckdb.blocking_rule_library import block_on
+from splink.duckdb.linker import DuckDBLinker
+
+pd.options.display.max_columns = None
+pd.options.display.max_colwidth = None
+df_pp = pd.read_parquet("splink_in/price_paid.parquet")
+df_epc = pd.read_parquet("splink_in/epc.parquet")
+
+
+num_1_comparison = {
+    "output_column_name": "numeric_token_1",
+    "comparison_levels": [
+        cll.null_level("numeric_token_1"),
+        {
+            "sql_condition": '"numeric_token_1_l" = "numeric_token_1_r"',
+            "label_for_charts": "Exact match",
+            "tf_adjustment_column": "numeric_token_1",
+            "tf_adjustment_weight": 1.0,
+        },
+        {
+            "sql_condition": '"numeric_token_2_l" = "numeric_token_1_r"',
+            "label_for_charts": "Exact match inverted numbers",
+            "tf_adjustment_column": "numeric_token_1",
+            "tf_adjustment_weight": 1.0,
+        },
+        cll.else_level(),
+    ],
+    "comparison_description": "numeric_token_1",
+}
+
+
+num_2_comparison = {
+    "output_column_name": "numeric_token_2",
+    "comparison_levels": [
+        cll.null_level("numeric_token_2"),
+        {
+            "sql_condition": '"numeric_token_2_l" = "numeric_token_2_r"',
+            "label_for_charts": "Exact match",
+            "tf_adjustment_column": "numeric_token_2",
+            "tf_adjustment_weight": 1.0,
+        },
+        {
+            "sql_condition": '"numeric_token_1_l" = "numeric_token_2_r"',
+            "label_for_charts": "Exact match inverted numbers",
+            "tf_adjustment_column": "numeric_token_2",
+            "tf_adjustment_weight": 1.0,
+        },
+        cll.else_level(),
+    ],
+    "comparison_description": "numeric_token_2",
+}
+
+num_3_comparison = {
+    "output_column_name": "numeric_token_3",
+    "comparison_levels": [
+        cll.null_level("numeric_token_3"),
+        {
+            "sql_condition": '"numeric_token_3_l" = "numeric_token_3_r"',
+            "label_for_charts": "Exact match",
+            "tf_adjustment_column": "numeric_token_3",
+            "tf_adjustment_weight": 1.0,
+        },
+        {
+            "sql_condition": '"numeric_token_2_l" = "numeric_token_3_r"',
+            "label_for_charts": "Exact match 2",
+            "tf_adjustment_column": "numeric_token_3",
+            "tf_adjustment_weight": 1.0,
+        },
+        cll.else_level(),
+    ],
+    "comparison_description": "numeric_token_3",
+}
+
+# This pretty gnarly SQL calculates:
+# 1. The product of the relative frequencies of matching tokens
+# 2. Then divides by the product of the relative frequencies of non-matching tokens
+# But non-matching items are weighted lower than matching (the 0.25)
+calc_tf_sql = """
+list_reduce(
+    list_prepend(
+        1.0,
+        list_transform(
+            array_filter(
+                token_relative_frequency_arr_l,
+                y -> array_contains(
+                    array_intersect(
+                        list_transform(token_relative_frequency_arr_l, x -> x.token),
+                        list_transform(token_relative_frequency_arr_r, x -> x.token)
+                    ),
+                    y.token
+                )
+            ),
+            x -> x.relative_frequency
+        )
+    ),
+    (p, q) -> p * q
+)
+*
+list_reduce(
+    list_prepend(
+        1.0,
+        list_transform(
+            list_concat(
+                array_filter(
+                    token_relative_frequency_arr_l,
+                        y -> not array_contains(
+                                list_transform(token_relative_frequency_arr_r, x -> x.token),
+                                y.token
+                            )
+                ),
+                array_filter(
+                    token_relative_frequency_arr_r,
+                        y -> not array_contains(
+                                list_transform(token_relative_frequency_arr_l, x -> x.token),
+                                y.token
+                            )
+                )
+            ),
+
+            x -> x.relative_frequency
+        )
+    ),
+    (p, q) -> p / q^0.25
+)
+"""
+
+token_relative_frequency_arr = {
+    "output_column_name": "token_relative_frequency_arr",
+    "comparison_levels": [
+        cll.null_level("token_relative_frequency_arr"),
+        {
+            "sql_condition": f"{calc_tf_sql} < 1e-10",
+            "label_for_charts": "<1e-10",
+        },
+        {
+            "sql_condition": f"{calc_tf_sql} < 1e-8",
+            "label_for_charts": "<1e-8",
+        },
+        {
+            "sql_condition": f"{calc_tf_sql} < 1e-6",
+            "label_for_charts": "<1e-6",
+        },
+        {
+            "sql_condition": f"{calc_tf_sql} < 1e-4",
+            "label_for_charts": "<1e-4",
+        },
+        {
+            "sql_condition": f"{calc_tf_sql} < 1e-2",
+            "label_for_charts": "<1e-2",
+        },
+        {
+            "sql_condition": f"{calc_tf_sql} < 1",
+            "label_for_charts": "<1e-2",
+        },
+        {"sql_condition": "ELSE", "label_for_charts": "All other comparisons"},
+    ],
+    "comparison_description": "Exact match vs. Col within levenshtein thresholds 1, 2 vs. anything else",
+}
+
+other_tokens_comparison = {
+    "output_column_name": "common_tokens",
+    "comparison_levels": [
+        {
+            "sql_condition": '"common_tokens_l" IS NULL OR "common_tokens_r" IS NULL or length("common_tokens_l") = 0 or length("common_tokens_r") = 0',
+            "label_for_charts": "Null",
+            "is_null_level": True,
+        },
+        {
+            "sql_condition": '2*len(list_intersect("common_tokens_l", "common_tokens_r")) - len(list_distinct(list_concat("common_tokens_l", "common_tokens_r"))) >= 2',
+            "label_for_charts": "More matching tokens than non matching > 2",
+        },
+        {
+            "sql_condition": '2*len(list_intersect("common_tokens_l", "common_tokens_r")) - len(list_distinct(list_concat("common_tokens_l", "common_tokens_r"))) > 0',
+            "label_for_charts": "More matching tokens than non matching",
+        },
+        {"sql_condition": "ELSE", "label_for_charts": "All other comparisons"},
+    ],
+    "comparison_description": "Array intersection",
+}
+settings = {
+    "probability_two_random_records_match": 0.01,
+    "link_type": "link_only",
+    "blocking_rules_to_generate_predictions": [
+        block_on(["postcode"], salting_partitions=10),
+    ],
+    "comparisons": [
+        num_1_comparison,
+        num_2_comparison,
+        num_3_comparison,
+        token_relative_frequency_arr,
+        other_tokens_comparison,
+        # This is not needed but makes a human readable form of the address appear
+        # in the comparison viewer dashboard
+        cl.exact_match("address_concat"),
+    ],
+    "retain_intermediate_calculation_columns": True,
+    "source_dataset_column_name": "source_dataset",
+    "additional_columns_to_retain": ["address_concat"],
+}
+
+
+linker = DuckDBLinker([df_pp, df_epc], settings)
+cl.exact_match("address_concat").as_dict()
+
+linker.estimate_u_using_random_sampling(max_pairs=1e6)
+# linker.estimate_parameters_using_expectation_maximisation(block_on("postcode"))
+
+
+comparisons = linker._settings_obj.comparisons
+
+# Increase punishment for non-matching 'numeric' token
+comparisons[0].comparison_levels[3].m_probability = 0.001
+comparisons[0].comparison_levels[3].u_probability = 1.0
+
+
+# Override the parameter estiamtes to null
+#  to make sure the 'address concat' field has no effect on the model
+comparisons[5].comparison_levels[1].m_probability = 0.5
+comparisons[5].comparison_levels[1].u_probability = 0.5
+
+comparisons[5].comparison_levels[2].m_probability = 0.5
+comparisons[5].comparison_levels[2].u_probability = 0.5
+display(linker.match_weights_chart())
+
+
+df_predict = linker.predict()
+
+
+# The remainder of this script is just for inspecting the results
+
+sql = """
+select unique_id from df_epc
+where numeric_token_2 is not null or true
+"""
+unique_ids = list(duckdb.sql(sql).df()["unique_id"])
+
+uid = random.choice(unique_ids)
+
+sql = f"""
+select *
+from {df_predict.physical_name}
+where
+unique_id_l = '{uid}'
+order by match_weight desc
+
+"""
+
+record_pairs = linker.query_sql(sql)
+epc_uid = record_pairs.head(1)["unique_id_l"].values[0]
+pp_uid = record_pairs.head(1)["unique_id_r"].values[0]
+
+
+sql_get_epc = f"""
+select
+'epc' as source_dataset, LMK_KEY, address1, address2, address3, POSTTOWN,  postcode
+from read_parquet('./raw_address_data/adur_epc.parquet')
+where LMK_KEY = '{epc_uid}'
+
+"""
+display(duckdb.sql(sql_get_epc).df())
+
+
+sql_get_pp = f"""
+select 'price_paid' as source_dataset, transaction_unique_identifier, saon, paon,
+    street, locality, town_city,  postcode
+from read_parquet('./raw_address_data/price_paid_addresses.parquet')
+where transaction_unique_identifier = '{pp_uid}'
+"""
+display(duckdb.sql(sql_get_pp).df())
+
+
+sql_get_epc_2 = f"""
+select
+numeric_token_1, numeric_token_2,	numeric_token_3, address_concat,
+from df_epc
+where unique_id = '{epc_uid}'
+"""
+display(duckdb.sql(sql_get_epc_2).df())
+
+
+sql_get_pp_2 = f"""
+select
+numeric_token_1,	numeric_token_2,	numeric_token_3, address_concat,
+from df_pp
+where unique_id = '{pp_uid}'
+"""
+display(duckdb.sql(sql_get_pp_2).df())
+
+
+display(linker.waterfall_chart(record_pairs.to_dict(orient="records")))
+
+# Uncomment out the parts relevant to the comparison viewer to produce a
+# easily readable comparison viewer dashboard
+linker.comparison_viewer_dashboard(
+    df_predict, "comparison_viewer_dashboard.html", overwrite=True
+)
