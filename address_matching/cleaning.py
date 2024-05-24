1b2641c3ad320c3eb890ec043a2aeef64a79c251
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-05-24 14:09:24 +0100

    wip

--- address_matching/cleaning.py
+++ address_matching/cleaning.py
@@ -116,21 +116,77 @@ def tokenise_address_without_numbers(table_name: str) -> DuckDBPyRelation:
     return duckdb.sql(sql)
 
 
+def add_term_frequencies_to_address_tokens(table_name: str) -> DuckDBPyRelation:
+    # Compute relative term frequencies amongst the tokens
+    sql = f"""
+    WITH token_counts AS (
+        SELECT
+            token,
+            count(*)  / sum(count(*)) OVER() as rel_freq
+        FROM (
+            SELECT
+                unnest(address_without_numbers_tokenised) as token
+            FROM {table_name}
+        )
+        GROUP BY token
+        ORDER BY rel_freq DESC
+    ),
+    addresses_exploded AS (
+        SELECT
+            unique_id, unnest(address_without_numbers_tokenised) as token
+        FROM {table_name}
+    ),
+    address_groups AS (
+        SELECT addresses_exploded.*, token_counts.rel_freq
+        FROM addresses_exploded
+        LEFT JOIN token_counts
+        ON addresses_exploded.token = token_counts.token
+    ),
+    token_freq_lookup AS (
+    SELECT
+        unique_id,
+        -- list(struct_pack(token := token, rel_freq := rel_freq)) as token_rel_freq_arr
+        list_zip(list(token), list(rel_freq))  as token_rel_freq_arr
+    FROM address_groups
+    GROUP BY unique_id)
+    SELECT
+        d.* exclude (address_without_numbers_tokenised), r.token_rel_freq_arr
+    from
+    {table_name} as d
+    inner join token_freq_lookup as r
+    on d.unique_id = r.unique_id
+
+    """
+
+    return duckdb.sql(sql)
+
+
 def house_name_to_number_if_no_numbers(table_name: str) -> DuckDBPyRelation:
+
+    # Get first below freq
+    first_token = "list_filter(token_rel_freq_arr, x -> x.rel_freq < 0.01)"
+
     sql = f"""
     select
-        * exclude (numeric_token_1, address_without_numbers_tokenised),
-        case
-            when numeric_token_1 is null then address_without_numbers_tokenised[1]
-            else numeric_token_1
-            end as numeric_token_1,
-
-    case
-        when numeric_token_1 is null then address_without_numbers_tokenised[2:]
-        else address_without_numbers_tokenised
-    end
-    as address_without_numbers_tokenised
+    list_any_value({first_token}) as first_token,
+    *
     from {table_name}
     """
+
+    # sql = f"""
+    # select
+    #     * exclude (numeric_token_1, address_without_numbers_tokenised),
+    #     case
+    #         when numeric_token_1 is null then address_without_numbers_tokenised[1]
+    #         else numeric_token_1
+    #         end as numeric_token_1,
+
+    # case
+    #     when numeric_token_1 is null then address_without_numbers_tokenised[2:]
+    #     else address_without_numbers_tokenised
+    # end
+    # as address_without_numbers_tokenised
+    # from {table_name}
+    # """
 
     return duckdb.sql(sql)
