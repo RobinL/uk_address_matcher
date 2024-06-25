dbc10c17f8d02c62bad7387a3feb1050b11f109a
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-06-25 09:27:11 +0100

    improve performance

--- example_performance.py
+++ example_performance.py
@@ -1,11 +1,13 @@
 import duckdb
+import pandas as pd
 from IPython.display import display
 
 from uk_address_matcher.cleaning_pipelines import (
     clean_data_using_precomputed_rel_tok_freq,
 )
-from uk_address_matcher.display_results import distinguishability
-from uk_address_matcher.splink_model import get_pretrained_linker
+from uk_address_matcher.splink_model import _performance_predict
+
+pd.options.display.max_colwidth = 1000
 
 # -----------------------------------------------------------------------------
 # Step 1: Load in some example data
@@ -31,12 +33,11 @@ from uk_address_matcher.splink_model import get_pretrained_linker
 
 # Remove the limit statements to run the full dataset, the limit is just to speed up
 # the example
-con = duckdb.connect(database=":memory:")
 p_ch = "./example_data/companies_house_addresess_postcode_overlap.parquet"
 p_fhrs = "./example_data/fhrs_addresses_sample.parquet"
 con = duckdb.connect(database=":memory:")
-df_1 = con.read_parquet(p_ch).order("postcode").limit(100)
-df_2 = con.read_parquet(p_fhrs).order("postcode").limit(100)
+df_1 = con.read_parquet(p_ch).order("postcode")
+df_2 = con.read_parquet(p_fhrs).order("postcode")
 
 
 # -----------------------------------------------------------------------------
@@ -49,49 +50,52 @@ df_1_c = clean_data_using_precomputed_rel_tok_freq(df_1, con=con)
 df_2_c = clean_data_using_precomputed_rel_tok_freq(df_2, con=con)
 
 
-# All dfs going in here are of type DuckDBPyRelation
-# See note at end re: using precomputed term frequencies
-linker = get_pretrained_linker(
-    [df_1_c, df_2_c], con=con, include_full_postcode_block=True
+linker, predictions = _performance_predict(
+    [df_1_c, df_2_c],
+    con=con,
+    match_weight_threshold=-10,
+    output_all_cols=True,
+    include_full_postcode_block=True,
 )
-linker.cumulative_num_comparisons_from_blocking_rules_chart()
-
-df_predict = linker.predict(threshold_match_weight=-10)
-
 
 # ------------------------------------------------------------------------------------
 # Step 3: Inspect the results:
 # ------------------------------------------------------------------------------------
 
-sql = f"""
-select
+sql = """
+SELECT
+    match_probability,
+    match_weight,
+    concat_ws(' ', original_address_concat_l, postcode_l) AS address_l,
+    concat_ws(' ', original_address_concat_r, postcode_r) AS address_r,
     unique_id_l,
     unique_id_r,
     source_dataset_l,
-    source_dataset_r,
-    match_probability,
-    match_weight,
-    original_address_concat_l,
-    original_address_concat_r
-    from {df_predict.physical_name}
-where match_weight > 0.5
+    source_dataset_r
+FROM
+    predictions
+WHERE
+    match_weight > 0
+QUALIFY
+    row_number() OVER (
+        PARTITION BY unique_id_l ORDER BY match_weight DESC
+    ) = 1
+
+"""
+
+top_predict = con.sql(sql).df()
+
+display(top_predict.head())
+
+
+sql = """
+SELECT * FROM predictions WHERE match_weight > 0
+QUALIFY row_number() OVER (PARTITION BY unique_id_l ORDER BY match_weight DESC) = 1
 order by random()
-limit 10
+limit 3
 """
 
-res = linker.query_sql(sql)
-display(res)
-
-sql = f"""
-SELECT *
-FROM {df_predict.physical_name}
-WHERE match_weight > 1
-QUALIFY ROW_NUMBER() OVER (PARTITION BY unique_id_l ORDER BY match_weight DESC) = 1
-ORDER BY random()
-LIMIT 3
-"""
-
-recs = linker.query_sql(sql).to_dict(orient="records")
+recs = con.sql(sql).df().to_dict(orient="records")
 
 
 for rec in recs:
@@ -99,13 +103,3 @@ for rec in recs:
     print(rec["unique_id_l"], rec["original_address_concat_l"])
     print(rec["unique_id_r"], rec["original_address_concat_r"])
     display(linker.waterfall_chart([rec]))
-
-
-# ------------------------------------------------------------------------------------
-# Step 4: Categorise by distinguishability
-# ------------------------------------------------------------------------------------
-offset = 0
-limit = 20
-distinguishability(linker, df_predict, human_readable=True).iloc[
-    offset : offset + limit
-]
