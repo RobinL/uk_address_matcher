b613d53537253a85937fff8b99d763c43ebd17dc
Robin Linacre <robinlinacre@hotmail.com>
2025-02-22 13:51:43 +0000

    organise

--- uk_address_matcher/__init__.py
+++ uk_address_matcher/__init__.py
@@ -1,1 +1,17 @@
-__version__ = "0.0.1.dev11"
+__version__ = "1.0.0.dev01"
+
+from uk_address_matcher.linking_model.splink_model import get_linker
+from uk_address_matcher.cleaning.cleaning_pipelines import (
+    clean_data_on_the_fly,
+    clean_data_using_precomputed_rel_tok_freq,
+    get_numeric_term_frequencies_from_address_table,
+    get_address_token_frequencies_from_address_table,
+)
+
+__all__ = [
+    "get_linker",
+    "clean_data_on_the_fly",
+    "clean_data_using_precomputed_rel_tok_freq",
+    "get_numeric_term_frequencies_from_address_table",
+    "get_address_token_frequencies_from_address_table",
+]
