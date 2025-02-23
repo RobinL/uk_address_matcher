4db44899258cf3a40dd697ef5f6f99bc19a574be
Robin Linacre <robinlinacre@hotmail.com>
2025-02-23 06:38:06 +0000

    upgrade to latest sqlglot

--- uk_address_matcher/__init__.py
+++ uk_address_matcher/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0.dev03"
+__version__ = "1.0.0.dev04"
 
 from uk_address_matcher.linking_model.splink_model import get_linker
 from uk_address_matcher.cleaning.cleaning_pipelines import (
