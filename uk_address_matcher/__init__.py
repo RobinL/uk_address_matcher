08c1f420cf07d0a3d5ff325ef6ddb83045b12090
Robin Linacre <robinlinacre@hotmail.com>
2025-02-23 22:07:41 +0000

    fix annoying licence problem

--- uk_address_matcher/__init__.py
+++ uk_address_matcher/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0.dev08"
+__version__ = "1.0.0.dev09"
 
 from uk_address_matcher.linking_model.splink_model import get_linker
 from uk_address_matcher.cleaning.cleaning_pipelines import (
