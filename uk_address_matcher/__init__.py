eda6bd3fafedd8e83b338eb43252a0085d964649
Robin Linacre <robinlinacre@hotmail.com>
2025-03-14 09:35:42 +0000

    1.0.0.dev16

--- uk_address_matcher/__init__.py
+++ uk_address_matcher/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0.dev15"
+__version__ = "1.0.0.dev16"
 
 from uk_address_matcher.linking_model.splink_model import get_linker
 from uk_address_matcher.cleaning.cleaning_pipelines import (
